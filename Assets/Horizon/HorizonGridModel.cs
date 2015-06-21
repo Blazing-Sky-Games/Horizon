f2a965a2c351ecf46f696a549ddc94f6845e8850
Matthew Draper <draper_matthewjohn@yahoo.com>
2015-06-20 19:21:11 -0700

    in game grid

--- Assets/Horizon/HorizonGridModel.cs
+++ Assets/Horizon/HorizonGridModel.cs
@@ -4,7 +4,18 @@ using System.Collections;
 
 using Gamelogic.Grids;
 
-public class HorizonGridModel : MonoBehaviour 
+public class HorizonGridModel : GridBehaviour<RectPoint> 
 {
-	
+	//unity messages
+
+	//
+
+	public IGrid<HorizonCellView, RectPoint> CellViewGrid;
+
+	public override void InitGrid ()
+	{
+		CellViewGrid = Grid.CastValues<HorizonCellView, RectPoint>();
+	}
+
+	//grid specific messages
 }
